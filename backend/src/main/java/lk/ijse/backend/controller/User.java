72512af498496672cadc46f125f39e2903ecc77d
Amidu99 <amidushamika@gmail.com>
2024-08-25 09:24:21 +0530

    add user controller

---
+++ backend/src/main/java/lk/ijse/backend/controller/User.java
@@ -0,0 +1,11 @@
+package lk.ijse.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+public class User {
+    
+}
