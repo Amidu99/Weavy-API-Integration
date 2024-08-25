32a42aeef75abc9a5490dae5b7d2b76b1725af70
Amidu99 <amidushamika@gmail.com>
2024-08-25 09:31:28 +0530

    add AppConfig

---
+++ backend/src/main/java/lk/ijse/backend/config/AppConfig.java
@@ -0,0 +1,19 @@
+package lk.ijse.backend.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.OkHttpClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public OkHttpClient getOkHttpClient() {
+        return new OkHttpClient();
+    }
+
+    @Bean
+    public ObjectMapper getObjectMapper() {
+        return new ObjectMapper();
+    }
+}
